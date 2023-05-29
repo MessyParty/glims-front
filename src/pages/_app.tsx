845191310efe88c12e10f94cac3eb274c031530e
Lyle Son <calends99@gmail.com>
2023-05-29 19:13:02 +0900

    import 경로 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { Global } from "@emotion/react";
-import reset from "@/styles/reset";
 import { Libre_Baskerville } from "next/font/google";
 import { ThemeProvider } from "@mui/material";
-import theme from "@/styles/theme";
+import reset from "../styles/reset";
+import theme from "../styles/theme";
 
 const baskerville = Libre_Baskerville({ weight: "400", subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
