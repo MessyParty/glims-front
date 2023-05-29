5b5e016d8f1621cd1f9609215b0551203129a5e4
yxxnhx <tjdbsgk514@hanmail.net>
2023-05-29 19:19:42 +0900

    app 컴포넌트 nav, footer 연결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { Libre_Baskerville } from "next/font/google";
 import { ThemeProvider } from "@mui/material";
 import reset from "../styles/reset";
 import theme from "../styles/theme";
+import NavBar from "@/components/common/NavBar";
+import Footer from "@/components/common/Footer";
 
 const baskerville = Libre_Baskerville({ weight: "400", subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,7 +15,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={theme}>
         <Global styles={reset} />
         <main className={baskerville.className}>
+          <NavBar />
           <Component {...pageProps} />
+          <Footer />
         </main>
       </ThemeProvider>
     </RecoilRoot>
