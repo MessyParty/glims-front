ca7c6a70e8c23b6166072d63d7512734c4f0de51
yxxnhx <tjdbsgk514@hanmail.net>
2023-05-29 17:47:02 +0900

    footer 추가

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,27 @@
+// import { ERROR_PAGE_REGEX } from "@/constants/regex";
+import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+
+const Footer = () => {
+  const router = useRouter();
+
+  /* TODO
+  if (ERROR_PAGE_REGEX.test(router.pathname)) return null;
+  */
+  return (
+    <FooterContainer>
+      <p>Copyright 2023 by glims. All rights reserved.</p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
+
+const FooterContainer = styled.footer`
+  text-align: center;
+  border-top: 1px solid #000;
+
+  & p {
+    padding: 1.5rem 0;
+  }
+`;
