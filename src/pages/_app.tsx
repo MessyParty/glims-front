710587bab32315d23b64ec9a6b462aa29500ab81
Lyle Son <calends99@gmail.com>
2023-05-29 19:09:10 +0900

    글로벌 스타일 수정 (폰트 포함)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,20 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { Global } from "@emotion/react";
 import reset from "@/styles/reset";
+import { Libre_Baskerville } from "next/font/google";
+import { ThemeProvider } from "@mui/material";
+import theme from "@/styles/theme";
 
+const baskerville = Libre_Baskerville({ weight: "400", subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Global styles={reset} />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Global styles={reset} />
+        <main className={baskerville.className}>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
     </RecoilRoot>
   );
 }
