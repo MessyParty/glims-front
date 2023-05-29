4b3e1d7ee796ccd33d621c98fff87022bdcbb3f0
yxxnhx <tjdbsgk514@hanmail.net>
2023-05-29 17:50:36 +0900

    app 기본 설정 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,57 @@
+import "@/styles/globals.css";
+import reset from "@/styles/reset";
+import theme from "@/styles/theme";
+
+import { useState } from "react";
 import type { AppProps } from "next/app";
+import {
+  QueryClientProvider,
+  QueryClient,
+  Hydrate,
+} from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
+import { CookiesProvider } from "react-cookie";
+import { Global, ThemeProvider } from "@emotion/react";
+
+import styled from "@emotion/styled";
+
+import axios from "axios";
+import NavBar from "@/components/common/NavBar";
+import Footer from "@/components/common/Footer";
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // ssr 적용시를 위한 useState
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { refetchOnWindowFocus: false } },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <CookiesProvider>
+            <ThemeProvider theme={theme}>
+              <Global styles={reset} />
+              <Container>
+                <NavBar />
+                <Component {...pageProps} />
+                <Footer />
+              </Container>
+            </ThemeProvider>
+          </CookiesProvider>
+        </RecoilRoot>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
+
+const Container = styled.div`
+  max-width: 1180px;
+  height: 100vh;
+  margin: 0 auto;
+`;
