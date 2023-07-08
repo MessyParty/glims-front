24b92a77d67cc3e7ac5aa3939b9451d7a4a35b39
yxxnhx <tjdbsgk514@hanmail.net>
2023-07-09 07:35:25 +0900

    리뷰 crud 수정

--- src/apis/review.ts
+++ src/apis/review.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 import {
+  ReveiwResponse,
   Review,
   ReviewListType,
   ReviewParameterType,
 } from "./interfaces/review.interface";
+import api from ".";
 
 export const getAllReview = async (
   params: ReviewParameterType,
@@ -18,9 +20,41 @@ export const getAllReview = async (
 };
 
 export const getReview = async (id: string): Promise<Review> => {
-  const { data } = await axios.get<Review>(
+  const { data } = await api.get<Review>(
     `https://dev.glims.store/api/v1/reviews/${id}`,
   );
 
+  return data;
+};
+
+export const deleteReview = async (id: string) => {
+  await api.delete(`https://dev.glims.store/api/v1/reviews/${id}`);
+};
+
+export const createReview = async (payload: ReveiwResponse) => {
+  const { data } = await api.post(`https://dev.glims.store/api/v1/reviews`, {
+    ...payload,
+  });
+  return data;
+};
+
+export const updateReview = async (id: string, payload: ReveiwResponse) => {
+  const { data } = await api.patch(
+    `https://dev.glims.store/api/v1/reviews/${id}`,
+    {
+      ...payload,
+    },
+  );
+  return data;
+};
+
+export const createReviewPhoto = async (uuid: string, body: FormData) => {
+  const { data } = await api.post(
+    `https://dev.glims.store/api/v1/reviews/photos/${uuid}`,
+    body,
+    {
+      headers: { "Content-Type": "multipart/form-data" },
+    },
+  );
   return data;
 };
