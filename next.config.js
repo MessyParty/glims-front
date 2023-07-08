1969e9b97aa02972be373b424f435c0a0c5e4adb
yxxnhx <tjdbsgk514@hanmail.net>
2023-07-09 07:34:27 +0900

    이미지 도메인 추가

--- next.config.js
+++ next.config.js
@@ -10,6 +10,22 @@ const nextConfig = {
 
     return config;
   },
+  images: {
+    domains: ["example.com", "glimsbucket.s3.ap-northeast-2.amazonaws.com"],
+  },
+  rewrites: async () => {
+    return [
+      {
+        // login으로 시작하는 url을 만나면 dest로 프록싱 해주는 설정 입니다
+        source: "/login/:paths*",
+        destination: "https://dev.glims.store/login/:paths*",
+      },
+      {
+        source: "/:path*",
+        destination: "https://dev.glims.store/api/v1/:path*",
+        has: [{ type: "header", key: "x-rewrite-target", value: "true" }],
+      },
+    ];
+  },
 };
-
 module.exports = nextConfig;
