65afa1dbcf968f13c6cda7b2e70b22244b52d080
yxxnhx <tjdbsgk514@hanmail.net>
2023-07-27 02:15:16 +0900

    이미지 링크 도메인 설정 추가

--- next.config.js
+++ next.config.js
@@ -11,7 +11,21 @@ const nextConfig = {
     return config;
   },
   images: {
-    domains: ["example.com", "glimsbucket.s3.ap-northeast-2.amazonaws.com"],
+    domains: [
+      "example.com",
+      "glimsbucket.s3.ap-northeast-2.amazonaws.com",
+      "www.byredo.com",
+      "www.chanel.com",
+      "www.aesop.com",
+      "www.jomalone.co.kr",
+      "cdn.shopify.com",
+      "assets.hermes.com",
+      "www.tamburins.com",
+      "www.byredo.com",
+      "i5.walmartimages.com",
+      "m.media-amazon.com",
+      "picsum.photos",
+    ],
   },
   rewrites: async () => {
     return [
