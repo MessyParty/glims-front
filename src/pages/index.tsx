feafd2ffd0ee29e75ee7eadd81b925d80c2d0cc1
yxxnhx <tjdbsgk514@hanmail.net>
2023-07-27 02:19:20 +0900

    불필요한 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
+import React from "react";
 import styled from "@emotion/styled";
-import useProfile from "@/hooks/queries/useProfile";
-import useMyReviews from "@/hooks/queries/useMyReviews";
-import { useRecoilValue } from "recoil";
-import { loginState } from "@/recoil/login";
-import { useBestReviews } from "@/hooks/queries/useReivew";
 import useModal from "@/hooks/useModal";
 import { MODAL_KEYS } from "@/constants/modalKeys";
 import Modal from "@/components/common/Modal";
-import React from "react";
 import SearchModal from "@/components/view/search/SearchModal";
-import { useRandomPerfume } from "@/hooks/queries/usePerfume";
 import LeftSideBox from "@/components/view/main/LeftSideBox";
 import RightSideBox from "@/components/view/main/RightSideBox";
 
